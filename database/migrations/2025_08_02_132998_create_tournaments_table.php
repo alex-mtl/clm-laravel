c90404674a1e14521a8b323cc71d18332684be93
Andrey <andrej.bockow@yandex.ru>
2025-11-06 23:46:34 +0300

    [fix_migrations] поправил миграции

---
+++ database/migrations/2025_08_02_132998_create_tournaments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('tournaments')) {
+            Schema::create('tournaments', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('club_id')->constrained()->onDelete('cascade');
+                $table->string('name');
+                $table->date('date_start');
+                $table->date('date_end');
+                $table->string('logo')->nullable();
+                $table->string('banner')->nullable();
+                $table->string('stream_banner')->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tournaments');
+    }
+};
